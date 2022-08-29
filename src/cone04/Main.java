372f36422fd70e1d8e73b78e7fd22f119dc6fb21
brycedd <trcat@outlook.com>
2022-08-30 00:31:06 +0800

    调整格式

--- src/cone04/Main.java
+++ src/cone04/Main.java
@@ -1,10 +1,11 @@
 package cone04;
 
+
 /**
  * @author Bryce_dd 2022/8/29 22:36
- *
+ * <p>
  * 猫狗队列
- *
+ * <p>
  * 实现猫狗队列的方法：add,pollAll, pollDog, pollCat, isEmpty, isDogEmpty, isCatEmpty
  */
 public class Main {
